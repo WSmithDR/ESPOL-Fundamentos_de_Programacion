cca5de202af915f23ebcc62e2a61d7b765c60fba
WagnerSmith <wagnersmith123@hotmail.com>
2023-10-21 12:54:01 -0500

    plantilla

--- Practica.py
+++ Practica.py
@@ -2,4 +2,14 @@ numeros = [34, 56, 75, 56, 43, 45, 56, 75]
 
 palabras = ["Quimica", "Algebra", "Fisica", "Biologia"]
 
+wagner = "Wagner Smith Dueñas Remache"
+
+dody = "Dody Salim Dueñas Remache"
+
+digits = "5454545454545"
+
+t = [[7, 3], [3, 6], [9, 8], [5, 4]]
+
+
+a = [[7, "Algebra"], [3, "Física"], [9, "Química"]]
 
