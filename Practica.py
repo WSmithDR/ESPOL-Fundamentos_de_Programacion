3182dfaa39f4f44e503feedca00772be6c9d796a
WagnerSmith <wagnersmith123@hotmail.com>
2023-10-21 13:18:23 -0500

    plantilla

--- Practica.py
+++ Practica.py
@@ -6,10 +6,20 @@ wagner = "Wagner Smith Dueñas Remache"
 
 dody = "Dody Salim Dueñas Remache"
 
+todojunto = "porquetodojuntoseescribeseparadoyseparadoseescribetodojunto"
+
 digits = "5454545454545"
+
+mayuscula = "AQUÍ TODOS LOS CARACTERES SON MAYUSCULAS"
+
+minuscula = "aquí todos los caracteres son minusculas"
+
+centrado = "                              asdfasdfasdfa asdfasdfasdfasdfa asdfas                     "
+centrado_n = "nnnnnnnnnnnnnnnnnnnnnnnasdfasdfasdfa asdfasdfasdfasdfa asdfasnnnnnnnnnnn"
 
 t = [[7, 3], [3, 6], [9, 8], [5, 4]]
 
 
 a = [[7, "Algebra"], [3, "Física"], [9, "Química"]]
+
 
