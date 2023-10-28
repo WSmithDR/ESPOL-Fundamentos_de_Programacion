b47e608d99391114428e68bc46b137cef2782a88
WagnerSmith <wagnersmith123@hotmail.com>
2023-10-27 20:10:02 -0500

    plantilla

--- ..py
+++
@@ -1,3 +0,0 @@
-primerParcial = input("Inserte la nota del primer parcial en el formato n/100 : ")
-segundoParcial = input("Inserte la nota del segundo parcial en el formato n/100 : ")
-practico = input("Inserte la nota del practico en el formato n/100 : ")
