43ff3cc9818133b94068aa9b05f5208620f10ff8
WagnerSmith <wagnersmith123@hotmail.com>
2023-12-02 10:06:24 -0500

    boludo

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 dic = {123:["Anita", 25], 234:["Elena", 34], 456:["Carmen", 45]}
+
+nombres = ["Wagner", "Dody", "Felix", "Raquel"]
+
+edades = [22, 20, 70, 53]
+
+familia = {"Juan":{"María", "Pedro"}, "Carlos":{"Susy", "Jorge", "Pedro"}, "Carmen":{"Lisa"}}
+
