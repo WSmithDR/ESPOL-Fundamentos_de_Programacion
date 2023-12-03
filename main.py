5ac5d4c751325e3b1cca98920ba1812928791ed4
WagnerSmith <wagnersmith123@hotmail.com>
2023-12-03 06:21:23 -0500

    profesores

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ edades = [22, 20, 70, 53]
 
 familia = {"Juan":{"María", "Pedro"}, "Carlos":{"Susy", "Jorge", "Pedro"}, "Carmen":{"Lisa"}}
 
+profesores = {123:{"Algebra": 5, "Física": 3}, 234:{"Algebra":2, "Fisica":4, "Quimica": 3}, 326:{"Física": 2}}
