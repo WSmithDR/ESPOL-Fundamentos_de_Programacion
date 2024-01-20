35154a3d93943676eb6e298a0c1d78dec9b1530b
WSmithDR <wagnersmith123@hotmail.com>
2024-01-20 15:41:20 -0500

    creando plantilla

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+nombres= np.array(["Carmen", "Juan","María", "Pedro"])
+materias = np.array(["Algebra", "Física", "Química"])
+calificaciones = np.array([[70, 60, 85], 
+                           [85, 90, 68],
+                           [72, 80, 58],
+                           [65, 78, 92]])
+
+print(calificaciones)
